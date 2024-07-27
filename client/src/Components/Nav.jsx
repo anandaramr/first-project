6facad4bf60a52e410888bed6c984350d8327c45
devika-krishnan <dkrishnanm@gmail.com>
2024-07-27 23:33:49 +0530

    add cover

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -4,10 +4,12 @@ function Nav()
 {
     return(
         <div className="">
-            <div className="bg-red-200 flex justify-between p-4 shadow-md cursor-default select-none">
+            
+            <div className=" bg-red-200 opacity-80 shadow-[0px_-10px_15px_black] flex justify-between p-4 cursor-default select-none">
+            
                 <div></div>
-                <p className="font-playwrite size inline-block text-3xl" >Mariot</p>
-                <span className="material-symbols-outlined flex items-center text-3xl">account_circle</span>
+                <p className="font-playwrite size inline-block text-3xl " >Mariot</p>
+                <span className="material-symbols-outlined flex items-center text-3xl ">account_circle</span>
             </div>
             
             <div className="group select-none cursor-default">
