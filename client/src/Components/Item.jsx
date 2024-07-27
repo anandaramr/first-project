b7d527950dad6dd39b5227735b726126c089dcaf
Anandaram R <anandaramr1915@gmail.com>
2024-07-27 17:44:00 +0530

    Refactor code

--- client/src/Components/Item.jsx
+++ client/src/Components/Item.jsx
@@ -1,8 +1,8 @@
 function Item({name,content})
 {
     return(
-        <div className="ml-[8%] px-8 py-2 basis-1/2 w-fit hover:bg-red-100 my-4   hover:rounded-3xl ">
-            <span className="material-symbols-outlined pr-2">{name}</span>
+        <div className="ml-[8%] px-8 py-2 basis-1/2 w-fit hover:bg-red-100 my-4 hover:rounded-3xl">
+            <span className="material-symbols-outlined pr-2 relative top-1">{name}</span>
             {content}
         </div>
     )
