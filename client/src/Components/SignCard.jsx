d8af5dc9d2dec794a970663c832921ad5e59580f
Pranav A Viswam <pranavviswam15@gmail.com>
2024-07-27 16:58:35 +0530

    Create login and sign up page

--- client/src/Components/SignCard.jsx
+++ client/src/Components/SignCard.jsx
@@ -1,26 +1,31 @@
 export default function SignCard()
 {
     return(
-        <div className="border-black border-2 rounded-lg shadow-lg font-poppins p-7 m-5 text-center flex flex-col justify-center w-auto h-auto bg-orange-200">
-            <h1 className="bold p-1">Welcome to <span className="font-playwrite">Mariot!</span></h1>
-            <div className="p-1">
-                <label htmlFor="name">User name:</label>
-                <input type="text" id="name"  placeholder="username" autoComplete="off" className="border-1 bg-inherit outline-none "></input>
-            </div>
-            <div className="p-1">
-                <label htmlFor="email">Email: </label>
-                <input type="text" id="email"  placeholder="x@gmail.com" autoComplete="off" className="border-1 bg-inherit outline-none"></input>
-            </div>
-            <div className="p-1">
-                <label htmlFor="pass">Password: </label>
-                <input type="text" id="pass"  placeholder="password" autoComplete="off" className="border-1 bg-inherit outline-none"></input>
-            </div>
-            <div className="p-1">
-                <label htmlFor="re-enter">Re-enter: </label>
-                <input type="text" id="re-enter"  placeholder="password" autoComplete="off" className="border-1 bg-inherit outline-none"></input>
+        <div className="flex flex-col justify-center items-center text-center">
+            <h1 className="font-playwrite font-bold text-3xl">Mariot!</h1>
+            <div className="border-black border-2 rounded-lg shadow-lg font-poppins px-20 p-7 m-5 text-center flex flex-col justify-center w-auto h-auto bg-orange-200">
+                <div className="p-1">
+                    
+                    <input type="text" id="name"  placeholder="username" autoComplete="off" className="border-b-2  w-72 px-2 py-2 bg-inherit outline-none "></input>
+                </div>
+                <div className="p-1">
+                    
+                    <input type="text" id="email"  placeholder="email" autoComplete="off" className="border-b-2 w-72 px-2 py-2 bg-inherit outline-none"></input>
+                </div>
+                <div className="p-1">
+                    
+                    <input type="password" id="pass"  placeholder="password" autoComplete="off" className="border-b-2 w-72 px-2 py-2 bg-inherit outline-none"></input>
+                </div>
+                <div className="p-1">
+                    
+                    <input type="password" id="re-enter"  placeholder="re-enter password" autoComplete="off" className="border-b-2 w-72 px-2 py-2 bg-inherit outline-none"></input>
+                </div>
+                <div>
+                    <button className="  mt-6 border-2  rounded-md hover:bg-black hover:text-white text-black border-black  py-1 px-2 ">Sign up</button>
+                </div>
+                
             </div>
             
-            <button className="   hover:bg-black hover:text-white text-black border-black  py-1 px-2 rounded">Sign up</button>
         </div>
     )
 }
