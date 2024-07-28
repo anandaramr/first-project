27826c5b29c63f4a8cba4ea1e7f3fa4ab7493643
devika-krishnan <dkrishnanm@gmail.com>
2024-07-28 19:10:10 +0530

    change cover

--- client/src/Components/Cover.jsx
+++ client/src/Components/Cover.jsx
@@ -1,7 +1,11 @@
+import Welcome from "./Welcome"
 function Cover()
 {
     return(
+        <div>
         <img className="absolute top-0 -z-20 blur-sm" src="../assets/back2.avif" alt="cover"/>
+        <Welcome/>
+        </div>
     )
 }
 export default Cover
---
+++ client/src/Components/Welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome()
+{
+    return(
+        <div className="flex-col flex justify-center items-center w-full h-[80svh] " >
+        <div className="font-poppins items-center  text-[#091222 text-4xl">Welcome to</div>
+        <div className="font-playwrite text-[#091222] font-semibold text-9xl">Mariot</div>
+        </div>
+    )
+}
+export default Welcome
