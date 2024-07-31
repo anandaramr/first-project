3a366f0b7517e94cff53156384e5b284440ed0e2
Anandaram R <anandaramr1915@gmail.com>
2024-07-31 22:07:12 +0530

    redo layout

--- client/src/Components/Element.jsx
+++ client/src/Components/Element.jsx
@@ -1,11 +1,15 @@
 function Card({heading,comp,source})
 {
     return(
-        <div className="font-raleway flex justify-evenly ">
-            <img className="size-[40%] rounded-3xl m-6" src={source} alt="heading"/>
-            <div className=" flex justify-start flex-col ">
+        <div className="font-raleway flex justify-evenly">
+            <img className="size-[40%] rounded-3xl m-6 hover:scale-[1.05] duration-200" src={source} alt="heading"/>
+            <div className=" flex justify-start flex-col items-start">
             <h2 className="font-semibold text-2xl mx-10 my-14">{heading}</h2>
             <p className="line-clamp-6 mx-10">{comp}</p>
+            <button className="group mx-10 my-5 p-2 font-poppins border-2 rounded border-slate-900 hover:bg-slate-900 hover:text-white duration-150">
+                {heading} 
+                <span className="group-hover:pl-3 gorup-hover:font-extrabold pl-2 duration-[inherit]">{">>"}</span>
+                </button>
             </div>
         </div>
     )
--- client/src/Components/Menu.jsx
+++ client/src/Components/Menu.jsx
@@ -3,8 +3,8 @@ import Item from './Item.jsx'
 function Menu()
 {
     return(
-        <div className="text-white  opacity-60 bg-gray-900 shadow-[-5px_16px_13px_white]  font-poppins text-lg   absolute -left-[20%]  h-full w-[20%] duration-200 ease-out pointer group-hover:left-0  ">
-            <div>
+        <div className="text-white opacity-60 bg-gray-900 font-poppins text-lg absolute -left-[20%] h-svh w-[20%] duration-200 ease-out pointer group-hover:left-0">
+            <div className='mt-10 space-y-6'>
             <Item name={"roofing"} content={"Auditorium"}/>
             <Item name={"night_shelter"} content={"Rooms"}/>
             <Item name={"Sprint"} content={"Sports"}/>
